12dd04039e3e726ffae13788fc13456659319dfe
RaulxAG <raul.arrgon@outlook.es>
2023-11-29 08:36:30 +0100

    Primera version: añadido formulario de datos bancarios

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos bancarios</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="number" name="numero" id="numero" placeholder="Numero de la tarjeta bancaria">
+        <input type="number" name="cseguridad" id="cseguridad" placeholder="CVV">
+        <label for="fechaexp">Fecha exp.</label>
+        <input type="date" name="fechaexp" id="fechaexp">
+        <input type="text" name="titular" id="titular" placeholder="Nombre del titular">
+    </form>
+</body>
+</html>
