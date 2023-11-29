dc34e1c164e00727f511b6fa9c48454d8e9203b8
RaulxAG <raul.arrgon@outlook.es>
2023-11-29 08:43:45 +0100

    Segunda version: campos obligatorios

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 </head>
 <body>
     <form action="" method="post">
-        <input type="number" name="numero" id="numero" placeholder="Numero de la tarjeta bancaria">
-        <input type="number" name="cseguridad" id="cseguridad" placeholder="CVV">
+        <input type="number" name="numero" id="numero" placeholder="Numero de la tarjeta bancaria" required>
+        <input type="number" name="cseguridad" id="cseguridad" placeholder="CVV" required>
         <label for="fechaexp">Fecha exp.</label>
-        <input type="date" name="fechaexp" id="fechaexp">
-        <input type="text" name="titular" id="titular" placeholder="Nombre del titular">
+        <input type="date" name="fechaexp" id="fechaexp" required>
+        <input type="text" name="titular" id="titular" placeholder="Nombre del titular" required>
     </form>
 </body>
 </html>
